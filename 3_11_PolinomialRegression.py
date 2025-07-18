cc00cb408b9238cdb0d1d05cdfc0e1be5ea65c69
irem saniye baran <155166534+iremsaniyebaran@users.noreply.github.com>
2025-07-18 13:34:18 +0300

    Add files via upload

---
+++ 3_11_PolinomialRegression.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 18 12:53:51 2025
+
+@author: irems
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprovessing import PolynomialFeatures
+
+X = 4 * np.random.rand(100, 1)
+y = 2 + 3 * X ** 2
+
+plt.scatter(X, y)
+
+
+poly_feat = PolynomialFeatures(degree=2)
+X_poly = poly_feat.fit_transform(X)
+
+poly_reg = LinearRegression()
+poly_reg.fit(X_poly, y)
+
+plt.scatter(X, y, color = "blue")
+
+X_test = np.linspace(0, 4, 100).reshape(-1, 1)
+X_test_poly = poly_feat.transform(X_test)
+y_pred = poly_reg.predict(X_test_poly)
+
+plt.plot(X_test, y_pred, color = "red")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Polinom Refresyon Modeli")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
